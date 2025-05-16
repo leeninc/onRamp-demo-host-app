8d163c12860d30c635f8e13954e3e6cd42949233
harsh-leen <harsh@leen.dev>
2025-05-16 15:34:32 +0530

    remove unused umport

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,5 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-import { VendorDataResponse } from './VendorDataResponse';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
